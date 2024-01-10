f43c32bfd7a32981d50be1795dd3fddaafdfe223
sheng wenxuan <1500862375@qq.com>
2024-01-10 11:23:53 +0800

    依赖变更

--- yuqing-manager/src/main/java/com/stonedt/YqAdminApplication.java
+++ yuqing-manager/src/main/java/com/stonedt/YqAdminApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableWechat
 public class YqAdminApplication {
 
 	public static void main(String[] args) {
